99d776a8cd2c27954dcb92f3f5819ffe6b582f2d
Rafael Corsi <rafael.corsi@insper.edu.br>
2023-10-09 23:35:33 -0300

    Update main.c

--- RTOS-OLED-Xplained-Pro/src/main.c
+++ RTOS-OLED-Xplained-Pro/src/main.c
@@ -5,6 +5,12 @@
 #include "gfx_mono_text.h"
 #include "sysfont.h"
 
+#define TASK_OLED_STACK_SIZE (1024 * 6 / sizeof(portSTACK_TYPE))
+#define TASK_OLED_STACK_PRIORITY (tskIDLE_PRIORITY)
+
+#define TASK_ALARM_STACK_SIZE (1024 * 6 / sizeof(portSTACK_TYPE))
+#define TASK_ALARM_STACK_PRIORITY (tskIDLE_PRIORITY)
+
 /** IOS **/
 #define LED_1_PIO PIOA
 #define LED_1_PIO_ID ID_PIOA
@@ -51,6 +57,11 @@ extern void xPortSysTickHandler(void);
 /************************************************************************/
 void io_init(void);
 
+typedef struct {
+	int id;
+	int status;
+} btn;
+
 /************************************************************************/
 /* RTOS application funcs                                               */
 /************************************************************************/
@@ -68,25 +79,145 @@ extern void vApplicationMallocFailedHook(void) {
 	configASSERT( ( volatile void * ) NULL );
 }
 
+
+SemaphoreHandle_t xSemaphoreEventAlarm;
+SemaphoreHandle_t xSemaphoreAfecAlarm;
+QueueHandle_t xQueueEvent;
+QueueHandle_t xQueueAdc;
+
+
+#define AFEC_POT AFEC0
+#define AFEC_POT_ID ID_AFEC0
+#define AFEC_POT_CHANNEL 0 // Canal do pino PD30
+
+/************************************************************************/
+/* prototypes                                                           */
+/************************************************************************/
+void io_init(void);
+void print_log(char *id, char *value);
+static void config_AFEC_pot(Afec *afec, uint32_t afec_id, uint32_t afec_channel,
+afec_callback_t callback);
+static void RTT_init(float freqPrescale, uint32_t IrqNPulses, uint32_t rttIRQSource);
+
 /************************************************************************/
 /* handlers / callbacks                                                 */
 /************************************************************************/
 
+
+void print_log(char *id, char *value) {
+	int sec = rtt_read_timer_value(RTT);
+	printf("[%s] %d %s \r\n", id, sec, value);
+}
+
 void but1_callback(void) {
-	printf("1 \n");
+	 	btn b = {.id = 1, .status = 0};
+	 	b.status = !pio_get(BUT_1_PIO, PIO_INPUT, BUT_1_IDX_MASK);
+	 	xQueueSendFromISR(xQueueEvent, &b, 0);
 }
 
 void but2_callback(void) {
-	printf("2 \n");
+		btn b = {.id = 2, .status = 0};
+		b.status = !pio_get(BUT_2_PIO, PIO_INPUT, BUT_2_IDX_MASK);
+		xQueueSendFromISR(xQueueEvent, &b, 0);
 }
 
 void but3_callback(void) {
-	printf("3 \n");
+	 	btn b = {.id = 3, .status = 0};
+	 	b.status = !pio_get(BUT_3_PIO, PIO_INPUT, BUT_3_IDX_MASK);
+	 	 	xQueueSendFromISR(xQueueEvent, &b, 0);
+}
+
+
+static void AFEC_pot_Callback(void) {
+	 	uint32_t ul_value;
+	 	ul_value = afec_channel_get_value(AFEC_POT, AFEC_POT_CHANNEL);
+	 	xQueueSendFromISR(xQueueAdc, &ul_value, 0);
 }
 
 /************************************************************************/
 /* TASKS                                                                */
 /************************************************************************/
+
+static void task_afec(void *pvParameters) {
+	config_AFEC_pot(AFEC_POT, AFEC_POT_ID, AFEC_POT_CHANNEL, AFEC_pot_Callback);
+	uint32_t ul_value;
+    RTT_init(1, 0, 0); 
+	char str[32];
+	int cntTh = 0;
+
+	for (;;) {
+		if (xQueueReceive(xQueueAdc, &ul_value, 0)) {
+			sprintf(str, "%d", ul_value);
+			print_log("AFEC ", str);
+
+			if (ul_value > 3000)
+			cntTh++;
+			else
+			cntTh = 0;
+		}
+
+		if (cntTh == 5)
+		xSemaphoreGive(xSemaphoreAfecAlarm);
+	}
+}
+
+
+static void task_event(void *pvParameters) {
+	io_init();
+	int btnData[3];
+	btn btnStatus;
+	char str[32];
+
+	for (int i = 0; i < 3; i++) {
+		btnData[i] = 0;
+	}
+
+	for (;;) {
+		if (xQueueReceive(xQueueEvent, &btnStatus, (TickType_t)200)) {
+			sprintf(str, "%d:%d", btnStatus.id, btnStatus.status);
+			print_log("EVENT", str);
+
+			btnData[btnStatus.id - 1] = btnStatus.status;
+		}
+
+		int cnt = 0;
+		for (int i = 0; i < 3; i++) {
+			cnt = btnData[i] + cnt;
+		}
+
+		if (cnt >= 2)
+		xSemaphoreGive(xSemaphoreEventAlarm);
+	}
+}
+
+static void task_alarm(void *pvParameters) {
+
+	int alarm_afec = 0;
+	int alarm_event = 0;
+
+	for (;;) {
+		if (xSemaphoreTake(xSemaphoreEventAlarm, 0)) {
+			print_log("ALARM", "EVENT");
+			gfx_mono_draw_string("EVENT", 0, 0, &sysfont);
+		}
+
+		if (xSemaphoreTake(xSemaphoreAfecAlarm, 0)) {
+			print_log("ALARM", "AFEC");
+			gfx_mono_draw_string("AFEC", 0, 16, &sysfont);
+		}
+
+		if (alarm_event)
+		pio_clear(LED_1_PIO, LED_1_IDX_MASK);
+		if (alarm_afec)
+		pio_clear(LED_2_PIO, LED_2_IDX_MASK);
+		vTaskDelay(100);
+		
+
+		pio_set(LED_1_PIO, LED_1_IDX_MASK);
+		pio_set(LED_2_PIO, LED_2_IDX_MASK);
+		vTaskDelay(100);
+	}
+}
 
 static void task_oled(void *pvParameters) {
 	gfx_mono_ssd1306_init();
@@ -160,6 +291,79 @@ void io_init(void) {
 	NVIC_SetPriority(BUT_3_PIO_ID, 4);
 }
 
+static void config_AFEC_pot(Afec *afec, uint32_t afec_id, uint32_t afec_channel,
+afec_callback_t callback) {
+	/*************************************
+	* Ativa e configura AFEC
+	*************************************/
+	/* Ativa AFEC - 0 */
+	afec_enable(afec);
+
+	/* struct de configuracao do AFEC */
+	struct afec_config afec_cfg;
+
+	/* Carrega parametros padrao */
+	afec_get_config_defaults(&afec_cfg);
+
+	/* Configura AFEC */
+	afec_init(afec, &afec_cfg);
+
+	/* Configura trigger por software */
+	afec_set_trigger(afec, AFEC_TRIG_SW);
+
+	/*** Configuracao específica do canal AFEC ***/
+	struct afec_ch_config afec_ch_cfg;
+	afec_ch_get_config_defaults(&afec_ch_cfg);
+	afec_ch_cfg.gain = AFEC_GAINVALUE_0;
+	afec_ch_set_config(afec, afec_channel, &afec_ch_cfg);
+
+	/*
+	* Calibracao:
+	* Because the internal ADC offset is 0x200, it should cancel it and shift
+	down to 0.
+	*/
+	afec_channel_set_analog_offset(afec, afec_channel, 0x200);
+
+	/***  Configura sensor de temperatura ***/
+	struct afec_temp_sensor_config afec_temp_sensor_cfg;
+
+	afec_temp_sensor_get_config_defaults(&afec_temp_sensor_cfg);
+	afec_temp_sensor_set_config(afec, &afec_temp_sensor_cfg);
+
+	/* configura IRQ */
+	afec_set_callback(afec, afec_channel, callback, 1);
+	NVIC_SetPriority(afec_id, 4);
+	NVIC_EnableIRQ(afec_id);
+}
+
+static void RTT_init(float freqPrescale, uint32_t IrqNPulses, uint32_t rttIRQSource) {
+
+	uint16_t pllPreScale = (int) (((float) 32768) / freqPrescale);
+	
+	rtt_sel_source(RTT, false);
+	rtt_init(RTT, pllPreScale);
+	
+	if (rttIRQSource & RTT_MR_ALMIEN) {
+		uint32_t ul_previous_time;
+		ul_previous_time = rtt_read_timer_value(RTT);
+		while (ul_previous_time == rtt_read_timer_value(RTT));
+		rtt_write_alarm_time(RTT, IrqNPulses+ul_previous_time);
+	}
+
+	/* config NVIC */
+	NVIC_DisableIRQ(RTT_IRQn);
+	NVIC_ClearPendingIRQ(RTT_IRQn);
+	NVIC_SetPriority(RTT_IRQn, 4);
+	NVIC_EnableIRQ(RTT_IRQn);
+
+	/* Enable RTT interrupt */
+	if (rttIRQSource & (RTT_MR_RTTINCIEN | RTT_MR_ALMIEN))
+	rtt_enable_interrupt(RTT, rttIRQSource);
+	else
+	rtt_disable_interrupt(RTT, RTT_MR_RTTINCIEN | RTT_MR_ALMIEN);
+	
+}
+
 /************************************************************************/
 /* main                                                                 */
 /************************************************************************/
@@ -171,10 +375,32 @@ int main(void) {
 	board_init();
 	configure_console();
 	io_init();
+	
+		xSemaphoreEventAlarm = xSemaphoreCreateBinary();
+		xSemaphoreAfecAlarm = xSemaphoreCreateBinary();
+		xQueueEvent = xQueueCreate(32, sizeof(btn));
+		xQueueAdc = xQueueCreate(32, sizeof(int));
+
+
+if (xTaskCreate(task_alarm, "alarm", TASK_ALARM_STACK_SIZE, NULL,
+TASK_ALARM_STACK_PRIORITY, NULL) != pdPASS) {
+	printf("Failed to create oled task\r\n");
+}
 
 	if (xTaskCreate(task_oled, "oled", TASK_OLED_STACK_SIZE, NULL, TASK_OLED_STACK_PRIORITY, NULL) != pdPASS) {
 		printf("Failed to create oled task\r\n");
 	}
+	
+	if (xTaskCreate(task_event, "event", TASK_ALARM_STACK_SIZE, NULL,
+	TASK_ALARM_STACK_PRIORITY, NULL) != pdPASS) {
+		printf("Failed to create oled task\r\n");
+	}
+	
+	if (xTaskCreate(task_afec, "afec", TASK_ALARM_STACK_SIZE, NULL,
+	TASK_ALARM_STACK_PRIORITY, NULL) != pdPASS) {
+		printf("Failed to create oled task\r\n");
+	}
+
 
 	vTaskStartScheduler();
 	while(1){}
